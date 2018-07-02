ba30f05d226d92b37ab982eaf1dd44e0ff2b10a6
Michail Yasonik <michail.yasonik@crowdstrike.com>
2018-07-01 19:53:18 -0700

    i18n setup

--- gulp/templates.js
+++ gulp/templates.js
@@ -4,17 +4,22 @@ var config = require('./config.js');
 var jade = require('gulp-jade');
 var jadeInheritance = require('gulp-jade-inheritance');
 var filter = require('gulp-filter');
+var en = require('../source/views/en.json');
+var ru = require('../source/views/ru.json');
 
 var jadeSrc = './' + config.src + 'views/';
 
-$.gulp.task('views', function() {
+$.gulp.task('views', ['russian'], function() {
     return $.gulp.src(jadeSrc + '**/*.jade')
         .pipe($.plumber({ errorHandler: $.notify.onError('<%= error.message %>') }))
         .pipe(jadeInheritance({ basedir: jadeSrc }))
         .pipe(jade({
             basedir: jadeSrc,
             pretty: true,
-            locals: { production: config.prod }
+            locals: {
+                text: en,
+                production: config.prod
+            },
         }))
         .pipe(filter(function(file) {
             var exclude;
@@ -24,4 +29,26 @@ $.gulp.task('views', function() {
             return !exclude.test(file.path);
         }))
         .pipe($.gulp.dest(config.dest));
+});
+
+$.gulp.task('russian', function() {
+    return $.gulp.src(jadeSrc + '**/*.jade')
+        .pipe($.plumber({ errorHandler: $.notify.onError('<%= error.message %>') }))
+        .pipe(jadeInheritance({ basedir: jadeSrc }))
+        .pipe(jade({
+            basedir: jadeSrc,
+            pretty: true,
+            locals: {
+                text: ru,
+                production: config.prod
+            },
+        }))
+        .pipe(filter(function(file) {
+            var exclude;
+            if (config.outputJadeIncludes) exclude = new RegExp('templates|mixins', 'g');
+            else exclude = new RegExp('templates|mixins|includes', 'g');
+
+            return !exclude.test(file.path);
+        }))
+        .pipe($.gulp.dest(config.dest + '/ru/'));
 });
